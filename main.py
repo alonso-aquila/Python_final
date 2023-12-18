5099705cdccb911f6f067018e69151546da4108a
Alonso <alonso.aquila@outlook.it>
2023-12-17 21:59:21 -0500

    Добавлено решение

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is final task Python web
+
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame(random.sample(['human', 'robot'] * 10, 20), columns={'Essence'})
+data.head()
